0eb6166a94b1ebd9561b2dd959082c661ade1b71
Jefferson Matheus <jefferson227@gmail.com>
2024-09-12 22:41:51 -0300

    Add test message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
-import MatchSimulator from './components/MatchSimulator/MatchSimulator';
+import "./App.css";
+import MatchSimulator from "./components/MatchSimulator/MatchSimulator";
 
 function App() {
   return (
     <div className="App">
       <MatchSimulator />
+      <p>This is running under Vite</p>
     </div>
   );
 }
