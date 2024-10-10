c50c5ee74e60ac609c5f6f71e422b4d99b1323cd
Jefferson Matheus <jefferson227@gmail.com>
2024-10-10 07:02:39 -0300

    Add order to the players' list

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,29 @@
 function addPlayerAttributes(players) {
   return players.map((player) => {
     player.id = crypto.randomUUID();
+    player.order = getPlayerOrder(player.position);
     return player;
   });
+}
+
+function getPlayerOrder(position) {
+  switch (position) {
+    case 'GK': {
+      return 0;
+    }
+    case 'DF': {
+      return 1;
+    }
+    case 'MF': {
+      return 2;
+    }
+    case 'FW': {
+      return 3;
+    }
+    default: {
+      return 0;
+    }
+  }
 }
 
 const utils = { addPlayerAttributes };
