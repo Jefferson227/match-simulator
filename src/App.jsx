19aba6aa1c6227f603794c6609870dec5defb105
Jefferson Matheus <jefferson227@gmail.com>
2024-10-12 12:15:38 -0300

    Set up i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import "./App.css";
-import AppProviders from "./providers/AppProviders";
-import MatchSimulator from "./components/MatchSimulator/MatchSimulator";
+import './App.css';
+import AppProviders from './providers/AppProviders';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+import MatchSimulator from './components/MatchSimulator/MatchSimulator';
 
 function App() {
   return (
-    <AppProviders>
-      <div className="App">
-        <MatchSimulator />
-      </div>
-    </AppProviders>
+    <I18nextProvider i18n={i18n}>
+      <AppProviders>
+        <div className="App">
+          <MatchSimulator />
+        </div>
+      </AppProviders>
+    </I18nextProvider>
   );
 }
 
---
+++ src/i18n.js
@@ -0,0 +1,19 @@
+// src/i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import ptBR from './locales/pt-BR.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    'pt-BR': { translation: ptBR },
+  },
+  lng: 'en', // default language
+  fallbackLng: 'en', // fallback if the language is not available
+  interpolation: {
+    escapeValue: false, // React already does escaping
+  },
+});
+
+export default i18n;
