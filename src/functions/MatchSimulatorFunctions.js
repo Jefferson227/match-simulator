867ff0152dd96b71f68898de66a8d06498090661
Jefferson Matheus <jefferson227@gmail.com>
2025-03-23 19:22:23 -0300

    Remove more unused functions

--- src/functions/MatchSimulatorFunctions.js
+++ src/functions/MatchSimulatorFunctions.js
@@ -56,59 +56,6 @@ function processBallAction(match) {
   }
 }
 
-function getCalculatedForces(sumForces) {
-  const random = getRandomDecimal(99);
-
-  if (parseInt(random.toFixed(0)) % 2 === 0) {
-    return sumForces * random;
-  }
-
-  return sumForces / random;
-}
-
-function getScorerTeamParam(homeTeam, visitorTeam) {
-  const sumForces = (team) =>
-    team.players.reduce((acc, player) => acc + player.strength, 0);
-
-  const calculatedForcesHomeTeam = getCalculatedForces(sumForces(homeTeam));
-  const calculatedForcesVisitorTeam = getCalculatedForces(
-    sumForces(visitorTeam)
-  );
-
-  return calculatedForcesHomeTeam > calculatedForcesVisitorTeam
-    ? 'home'
-    : calculatedForcesHomeTeam < calculatedForcesVisitorTeam
-    ? 'visitor'
-    : '';
-}
-
-function getScorerTeamData(param, homeTeam, visitorTeam) {
-  switch (param) {
-    case 'home':
-      return homeTeam;
-    case 'visitor':
-      return visitorTeam;
-    default:
-      return { team: null, setScore: () => {} };
-  }
-}
-
-function findScorer(team) {
-  if (!team) return null;
-
-  const percentagePosition = getRandomDecimal(100);
-  let position;
-
-  if (percentagePosition <= 2) position = 'DF';
-  else if (percentagePosition <= 10) position = 'MF';
-  else position = 'FW';
-
-  const players = team.players.filter((player) => player.position === position);
-  const randomIndex = Math.floor(getRandomDecimal(players.length));
-
-  return players[randomIndex] || null;
-}
-
 function movePlayers(match) {
   const moveDirection = (player, teamType) => {
     let columnChange = 0;
@@ -138,41 +85,6 @@ function movePlayers(match) {
   );
 }
 
-function runAction(time, setScorer, matches, increaseScore) {
-  matches.forEach((match) => {
-    // Any of the teams has a chance of scoring of 2.5%
-    const percentage = getRandomDecimal(100);
-    if (percentage >= 2.5) {
-      return;
-    }
-
-    // Getting which team will score
-    const scorerTeamParam = getScorerTeamParam(
-      match.homeTeam,
-      match.visitorTeam
-    );
-    if (scorerTeamParam === '') return;
-
-    const scorerTeam = getScorerTeamData(
-      scorerTeamParam,
-      match.homeTeam,
-      match.visitorTeam
-    );
-
-    // Getting the scorer in the scorer team
-    const scorer = findScorer(scorerTeam);
-
-    // Setting the scorer and updating the scoreboard
-    const goalScorer = {
-      playerName: scorer?.name || '',
-      time: time,
-    };
-
-    setScorer(match.id, goalScorer);
-    increaseScore(match.id, scorerTeam);
-  });
-}
-
 function getRandomDecimal(multiplier) {
   return parseFloat((Math.random() * multiplier).toFixed(2));
 }
@@ -190,7 +102,6 @@ function tickClock(time, setScorer, matches, increaseScore) {
     movePlayers(match);
     processBallAction(match);
   });
-  runAction(time, setScorer, matches, increaseScore);
 
   matches.forEach((match) => {
     if (match.latestGoal) {
