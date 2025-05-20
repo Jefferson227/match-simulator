52af72b652124d97f85e902628ea4ff18c059a12
Jefferson Matheus <jefferson227@gmail.com>
2025-05-20 07:55:25 -0300

    Update comments

--- src/functions/MatchSimulatorFunctions.ts
+++ src/functions/MatchSimulatorFunctions.ts
@@ -181,14 +181,14 @@ function endMatch(): void {
 function runMatchLogic(match: Match): void {
   /*
    * TODO: Implement the logic for each match
-   * - Calculate the teams' strength
+   * - Calculate the teams' overall strength
    * - Get the teams' morale
-   * - Get the teams' formations
+   * - Get the teams' attack, midfield and defense strength (sum of the strength of the players in each position in the team)
    * - Get the teams' moods (the mood of the team is random each time a match starts)
    *
    * The team morale is a number between 0 and 100 and it's build up according to the team's progress throughout the season.
    * The team formation has a different weight to be calculated.
-   * The team mood is the sum of a number between 0 and 100 set at the start of the match for each player in each team.
+   * The team mood is the average of a number between 0 and 100 set at the start of the match for each player in each team.
    */
 }
 
