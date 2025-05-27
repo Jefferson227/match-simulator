3f19dbb571c5601bed491b0870d9d56cfb46b4a0
Jefferson Matheus <jefferson227@gmail.com>
2025-05-27 08:19:33 -0300

    Adjust styling

--- src/components/MatchDetails.tsx
+++ src/components/MatchDetails.tsx
@@ -11,36 +11,39 @@ interface MatchDetailsProps {
 const MatchDetails: FC<MatchDetailsProps> = ({ match, scorers, onBack }) => {
   const { t } = useTranslation();
   return (
-    <div className="font-press-start w-full max-w-[400px] mx-auto mt-8 outline outline-4 outline-white bg-[#397a33] p-4">
+    <div className="font-press-start w-[350px] h-[596px] mx-auto mt-8 outline outline-4 outline-white bg-[#397a33] p-4">
       <div className="flex justify-between items-center border-b-2 border-white pb-4 mb-4">
         <div className="flex items-center">
           <div
-            className="w-[80px] h-[56px] flex items-center justify-center text-[28px] font-bold mr-4 outline outline-4"
+            className="w-[95px] h-[58px] border-[4px] box-content flex items-center justify-center text-[22px] mr-4"
             style={{
               backgroundColor: match.homeTeam.colors.background,
               color: match.homeTeam.colors.name,
-              outlineColor: match.homeTeam.colors.outline,
+              borderColor: match.homeTeam.colors.outline,
             }}
           >
             {match.homeTeam.abbreviation}
           </div>
         </div>
-        <div className="text-[28px] text-white font-bold">
+
+        <div className="text-[22px] text-white">
           {match.homeTeam.score}x{match.visitorTeam.score}
         </div>
+
         <div className="flex items-center">
           <div
-            className="w-[80px] h-[56px] flex items-center justify-center text-[28px] font-bold ml-4 outline outline-4"
+            className="w-[95px] h-[58px] border-[4px] box-content flex items-center justify-center text-[22px] ml-4"
             style={{
               backgroundColor: match.visitorTeam.colors.background,
               color: match.visitorTeam.colors.name,
-              outlineColor: match.visitorTeam.colors.outline,
+              borderColor: match.visitorTeam.colors.outline,
             }}
           >
             {match.visitorTeam.abbreviation}
           </div>
         </div>
       </div>
+
       <div className="flex flex-col gap-2 min-h-[180px] mb-8">
         {scorers.length === 0 ? (
           <div className="text-center text-white text-[16px]">
