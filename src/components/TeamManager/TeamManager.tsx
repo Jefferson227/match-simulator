9558479ce1610b48f8a64b4e2b4d41cd881f5d78
Jefferson Matheus <jefferson227@gmail.com>
2025-06-19 23:17:29 -0300

    Adjust pagination buttons
    Also adding a new button called "More Info"

--- src/components/TeamManager/TeamManager.tsx
+++ src/components/TeamManager/TeamManager.tsx
@@ -479,7 +479,7 @@ const TeamManager: React.FC = () => {
         <>
           <div className="flex w-[350px] justify-between gap-2 mx-auto">
             <button
-              className="w-1/2 border-4 py-2 px-3 leading-[19px] text-[16px]"
+              className="w-1/3 h-[70px] border-4 py-2 px-3 leading-[19px] text-[16px]"
               style={{
                 borderColor: '#e2e2e2',
                 backgroundColor: '#3c7a33',
@@ -490,10 +490,24 @@ const TeamManager: React.FC = () => {
               onClick={handlePrevPage}
               disabled={currentPage === 0}
             >
-              {t('teamManager.prevPage')}
+              {'<'}
             </button>
             <button
-              className="w-1/2 border-4 py-2 px-3 leading-[19px] text-[16px]"
+              className="w-1/3 h-[70px] border-4 py-2 px-3 leading-[19px] text-[16px]"
+              style={{
+                borderColor: '#e2e2e2',
+                backgroundColor: '#3c7a33',
+                color: '#e2e2e2',
+                opacity: 0.5,
+                cursor: 'not-allowed',
+              }}
+              onClick={() => {}}
+              disabled={true}
+            >
+              {t('teamManager.moreInfo')}
+            </button>
+            <button
+              className="w-1/3 h-[70px] border-4 py-2 px-3 leading-[19px] text-[16px]"
               style={{
                 borderColor: '#e2e2e2',
                 backgroundColor: '#3c7a33',
@@ -508,7 +522,7 @@ const TeamManager: React.FC = () => {
               onClick={handleNextPage}
               disabled={currentPage === totalPages - 1 || totalPages === 0}
             >
-              {t('teamManager.nextPage')}
+              {'>'}
             </button>
           </div>
           {selectedCount === 11 && (
