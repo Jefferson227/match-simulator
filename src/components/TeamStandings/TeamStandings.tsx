c4a0729ea50b49cf25320840fd24cd34e654ae97
Jefferson Matheus <jefferson227@gmail.com>
2025-07-06 02:22:05 -0300

    Adjust season complete text

--- src/components/TeamStandings/TeamStandings.tsx
+++ src/components/TeamStandings/TeamStandings.tsx
@@ -165,8 +165,12 @@ const TeamStandings: React.FC<TeamStandingsProps> = ({
         {championshipName}
       </div>
       <div className="text-center text-[14px] text-white mb-2 uppercase">
-        {championshipState.year} - Round {championshipState.currentRound} of{' '}
-        {totalRounds}
+        {!isSeasonComplete && (
+          <>
+            {championshipState.year} - Round {championshipState.currentRound} of{' '}
+            {totalRounds}
+          </>
+        )}
         {isSeasonComplete && (
           <span className="block text-[12px] text-yellow-300">
             SEASON COMPLETE!
