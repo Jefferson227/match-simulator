9db044d3ae17ae9322ad9bf457a7678f0c61f202
Jefferson Matheus <jefferson227@gmail.com>
2025-08-03 22:57:00 -0300

    Remove comments

--- src/services/promotionRelegationService.ts
+++ src/services/promotionRelegationService.ts
@@ -18,45 +18,6 @@ export const handlePromotionRelegationLogic = (
   updateChampionshipState: (championshipUpdateObject: ChampionshipUpdate) => void,
   championshipState: ChampionshipState
 ) => {
-  /*
-  # Main logic
-  - If promotion championship doesn't exist, keep promoted teams from current championship in the same championship
-  - If promotion championship exists:
-    - Move promoted teams from current championship to promotion championship
-    - Move relegated teams from promotion championship to current championship
-  - If relegation championship doesn't exist, keep relegated teams from current championship in the same championship
-  - If relegation championship exists:
-    - Move relegated teams from current championship to relegation championship
-    - Move promoted teams from relegation championship to current championship
-  - If human player team was promoted, set promotion championship as the current championship
-  - If human player team was relegated, set relegation championship as the current championship
-  - If human player team was neither promoted nor relegated, set current championship as the current championship
-  - Generate season match calendar for next season
-  */
-  /*
-  # Pseudo code
-  if (hasPromotionChampionship(currentChampionship)) {
-    movePromotedTeamsToPromotionChampionship(currentChampionship);
-    moveRelegatedTeamsToCurrentChampionship(promotionChampionship);
-  }
-
-  if (hasRelegationChampionship(currentChampionship)) {
-    moveRelegatedTeamsToRelegationChampionship(currentChampionship);
-    movePromotedTeamsToCurrentChampionship(relegationChampionship);
-  }
-
-  if (humanPlayerTeamWasPromoted) {
-    setChampionship(currentChampionship, promotionChampionship);
-  }
-
-  if (humanPlayerTeamWasRelegated) {
-    setChampionship(currentChampionship, relegationChampionship);
-  }
-
-  generateSeasonMatchCalendar(currentChampionship);
-  */
-
-  // TODO: If a team is promoted from série B to série A, séries A and C get updated, but not série B, and it needs to be fixed
   const currentChampionship = championshipState;
   let promotionResult = {} as PromotionResult;
   let newPromotionChampionshipConfig: ChampionshipConfig | undefined;
