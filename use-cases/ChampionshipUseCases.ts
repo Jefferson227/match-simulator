acc261102a72ce6ab4b6790114a4e8337a535c57
Jefferson Matheus <jefferson227@gmail.com>
2026-01-04 18:39:13 -0500

    Add TODO

--- use-cases/ChampionshipUseCases.ts
+++ use-cases/ChampionshipUseCases.ts
@@ -3,6 +3,7 @@ import { Championship } from '../core/models/Championship';
 
 function initChampionships(internalName: string): OperationResult<Championship> {
   // TODO: Create ChampionshipDTO
+  // TODO: Change the names of the methods in the OperationResult to setError and setSuccess
 
   const operationResult = new OperationResult({} as Championship).getError({} as Championship, {
     errorCode: 'implementation-missing',
