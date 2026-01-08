42d86a5b44ad132d0965328f2c062c8fa2927307
Jefferson Matheus <jefferson227@gmail.com>
2026-01-07 23:55:56 -0500

    Add game engine provider to the other providers

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,19 +1,25 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useMemo } from 'react';
 import { MatchProvider } from '../contexts/MatchContext';
 import { GeneralProvider } from '../contexts/GeneralContext';
 import { ChampionshipProvider } from '../contexts/ChampionshipContext';
+import { GameEngineProvider } from '../contexts/GameEngineContext';
+import { createInitialGameState } from '../../game-engine/initialGameState';
 
 interface AppProvidersProps {
   children: ReactNode;
 }
 
 const AppProviders: FC<AppProvidersProps> = ({ children }) => {
+  const initialState = useMemo(() => createInitialGameState(), []);
+
   return (
-    <GeneralProvider>
-      <ChampionshipProvider>
-        <MatchProvider>{children}</MatchProvider>
-      </ChampionshipProvider>
-    </GeneralProvider>
+    <GameEngineProvider initialState={initialState}>
+      <GeneralProvider>
+        <ChampionshipProvider>
+          <MatchProvider>{children}</MatchProvider>
+        </ChampionshipProvider>
+      </GeneralProvider>
+    </GameEngineProvider>
   );
 };
 
