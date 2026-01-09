7941c2bcc7076a4fef6140abc2d0038f62d42832
Jefferson Matheus <jefferson227@gmail.com>
2026-01-09 00:50:47 -0500

    Add ChampionshipService to the service layer

---
+++ core/services/ChampionshipService.ts
@@ -0,0 +1,20 @@
+import ChampionshipContainer from '../models/ChampionshipContainer';
+
+function initChampionships(championshipInternalName: string): ChampionshipContainer {
+  /**
+   * 1. Take the `championshipInternalName` and get the corresponding championship
+   *    from the JSON in `src/assets/championships.json` (file to be renamed), and
+   *    map it into a Championship object called `playableChampionship`.
+   * 2. If `playableChampionship.relegationChampionship` is not empty, get the
+   *    corresponding team and map it into a Championship object called
+   *    `relegationChampionship`.
+   * 3. If `playableChampionship.promotionChampionship` is not empty, get the
+   *    corresponding team and map it into a Championship object called
+   *    `promotionChampionship`.
+   * 4. Return a new ChampionshipContainer object containing all Championship
+   *    objects created.
+   */
+  return {} as ChampionshipContainer;
+}
+
+export default { initChampionships };
