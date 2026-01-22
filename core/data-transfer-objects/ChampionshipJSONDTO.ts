313941aa7e14a948e05ca1ecb2af1c111bdb530f
Jefferson Matheus <jefferson227@gmail.com>
2026-01-22 00:18:14 -0500

    Adjust ChampionshipJSONDTO class

--- core/data-transfer-objects/ChampionshipJSONDTO.ts
+++ core/data-transfer-objects/ChampionshipJSONDTO.ts
@@ -1,6 +1,6 @@
 import ChampionshipType from '../enums/ChampionshipType';
 
-type ChampionshipJSONDTO = {
+type DTO = {
   id: string;
   name: string;
   internalName: string;
@@ -12,5 +12,23 @@ type ChampionshipJSONDTO = {
   type: ChampionshipType;
   teams?: string[];
 };
+
+type Promotable = {
+  isPromotable: boolean;
+  numberOfPromotedTeams: number;
+  promotionChampionshipInternalName: string;
+};
+
+type Relegatable = {
+  isRelegatable: boolean;
+  numberOfRelegatedTeams: number;
+  relegationChampionshipInternalName: string;
+};
+
+type PromotableFields = { isPromotable: false } | ({ isPromotable: true } & Promotable);
+
+type RelegatableFields = { isRelegatable: false } | ({ isRelegatable: true } & Relegatable);
+
+type ChampionshipJSONDTO = DTO & PromotableFields & RelegatableFields;
 
 export default ChampionshipJSONDTO;
