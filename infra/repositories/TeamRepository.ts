127efac3511787099dbe7225e5e253ac9e738d1d
Jefferson Matheus <jefferson227@gmail.com>
2026-01-25 21:01:52 -0500

    Implement team mapping from JSON files

--- infra/repositories/TeamRepository.ts
+++ infra/repositories/TeamRepository.ts
@@ -1,5 +1,7 @@
 import TeamJSONDTO from '../../core/data-transfer-objects/TeamJSONDTO';
+import PlayerPosition from '../../core/enums/PlayerPosition';
 import { Team } from '../../core/models/Team';
+import { getRandomPlayerStrength } from '../../core/utils/Utils';
 
 const filePath = '../../assets/teams/';
 
@@ -13,8 +15,25 @@ async function getTeam(internalName: string): Promise<Team | undefined> {
     }
 
     const teamJSONDTO = (await response.json()) as TeamJSONDTO;
-    // TODO: Map teamJSONDTO into Team
-    const mappedTeam = {} as Team;
+    const mappedTeam: Team = {
+      id: crypto.randomUUID(),
+      fullName: teamJSONDTO.name,
+      shortName: teamJSONDTO.shortName,
+      abbreviation: teamJSONDTO.abbreviation,
+      colors: {
+        outline: teamJSONDTO.colors.outline,
+        background: teamJSONDTO.colors.background,
+        text: teamJSONDTO.colors.name,
+      },
+      players: teamJSONDTO.players.map((player) => ({
+        id: crypto.randomUUID(),
+        position: player.position as PlayerPosition,
+        name: player.name,
+        strength: getRandomPlayerStrength(teamJSONDTO.initialOverallStrength),
+      })),
+      morale: 50,
+      isControlledByHuman: false,
+    };
 
     return mappedTeam;
   } catch {
