d7c112d4ba89fe549058014754b11f3a79ca2a8e
Jefferson Matheus <jefferson227@gmail.com>
2026-01-27 00:10:48 -0500

    Implement getTeam function

--- infra/repositories/TeamRepository.ts
+++ infra/repositories/TeamRepository.ts
@@ -2,43 +2,36 @@ import TeamJSONDTO from '../../core/data-transfer-objects/TeamJSONDTO';
 import PlayerPosition from '../../core/enums/PlayerPosition';
 import { Team } from '../../core/models/Team';
 import { getRandomPlayerStrength } from '../../core/utils/Utils';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 const filePath = '../../assets/teams/';
 
-async function getTeam(internalName: string): Promise<Team | undefined> {
-  try {
-    const path = `${filePath}${internalName}.json`;
-    const response = await fetch(path);
-
-    if (!response.ok) {
-      return undefined;
-    }
-
-    const teamJSONDTO = (await response.json()) as TeamJSONDTO;
-    const mappedTeam: Team = {
+function getTeam(internalName: string): Team | undefined {
+  const resolvedPath = path.resolve(__dirname, filePath, `${internalName}.json`);
+  const fileContents = readFileSync(resolvedPath, 'utf-8');
+  const teamJSONDTO = JSON.parse(fileContents) as TeamJSONDTO;
+  const mappedTeam: Team = {
+    id: crypto.randomUUID(),
+    fullName: teamJSONDTO.name,
+    shortName: teamJSONDTO.shortName,
+    abbreviation: teamJSONDTO.abbreviation,
+    colors: {
+      outline: teamJSONDTO.colors.outline,
+      background: teamJSONDTO.colors.background,
+      text: teamJSONDTO.colors.name,
+    },
+    players: teamJSONDTO.players.map((player) => ({
       id: crypto.randomUUID(),
-      fullName: teamJSONDTO.name,
-      shortName: teamJSONDTO.shortName,
-      abbreviation: teamJSONDTO.abbreviation,
-      colors: {
-        outline: teamJSONDTO.colors.outline,
-        background: teamJSONDTO.colors.background,
-        text: teamJSONDTO.colors.name,
-      },
-      players: teamJSONDTO.players.map((player) => ({
-        id: crypto.randomUUID(),
-        position: player.position as PlayerPosition,
-        name: player.name,
-        strength: getRandomPlayerStrength(teamJSONDTO.initialOverallStrength),
-      })),
-      morale: 50,
-      isControlledByHuman: false,
-    };
+      position: player.position as PlayerPosition,
+      name: player.name,
+      strength: getRandomPlayerStrength(teamJSONDTO.initialOverallStrength),
+    })),
+    morale: 50,
+    isControlledByHuman: false,
+  };
 
-    return mappedTeam;
-  } catch {
-    return undefined;
-  }
+  return mappedTeam;
 }
 
 export default { getTeam };
