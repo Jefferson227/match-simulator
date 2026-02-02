b5be2f6666cfb110f086cdcabb19ab0f4469f6ed
Jefferson Matheus <jefferson227@gmail.com>
2026-02-02 00:10:06 -0500

    Update test cases

--- tests/use-cases/ChampionshipUseCases.test.ts
+++ tests/use-cases/ChampionshipUseCases.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, it } from '@jest/globals';
 
-import { initChampionships } from '../../use-cases/ChampionshipUseCases';
+import {
+  initChampionships,
+  getChampionshipInternalNames,
+} from '../../use-cases/ChampionshipUseCases';
 
 describe('initChampionships', () => {
   it('returns a successful result from ChampionshipService', () => {
@@ -12,5 +15,17 @@ describe('initChampionships', () => {
     const brasileiraoSerieB = 'brasileirao-serie-b';
     const result = initChampionships(brasileiraoSerieB);
     expect(result.getResult().playableChampionship.internalName).toBe(brasileiraoSerieB);
+  });
+});
+
+describe('getChampionshipInternalNames', () => {
+  it('returns a successful result from getChampionshipInternalNames', () => {
+    const result = getChampionshipInternalNames();
+    expect(result.succeeded).toBeTruthy();
+  });
+
+  it('returns a list of championship internal names', () => {
+    const result = getChampionshipInternalNames();
+    expect(result.getResult().length > 0).toBeTruthy();
   });
 });
