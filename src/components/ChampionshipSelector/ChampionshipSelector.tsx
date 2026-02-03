5cd61977c43a0f44dfc01dce27f77566c64d631a
Jefferson Matheus <jefferson227@gmail.com>
2026-02-03 00:39:08 -0500

    Get championship internal name and name

--- src/components/ChampionshipSelector/ChampionshipSelector.tsx
+++ src/components/ChampionshipSelector/ChampionshipSelector.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { GeneralContext } from '../../contexts/GeneralContext';
 import { useGameEngine } from '../../contexts/GameEngineContext';
 import { useGameState } from '../../services/useGameState';
-import { getChampionshipInternalNames } from '../../../use-cases/ChampionshipUseCases';
+import { getChampionships } from '../../../use-cases/ChampionshipUseCases';
 
 const CHAMPIONSHIPS_PER_PAGE = 6;
 
@@ -16,8 +16,7 @@ const ChampionshipSelector: React.FC = () => {
   const engine = useGameEngine();
   const state = useGameState(engine);
 
-  // TODO: Get the internal name and the name to be displayed
-  const result = getChampionshipInternalNames();
+  const result = getChampionships();
   if (!result.succeeded)
     engine.dispatch({ type: 'SET_ERROR_MESSAGE', errorMessage: result.error.message });
 
@@ -54,18 +53,18 @@ const ChampionshipSelector: React.FC = () => {
       <h1 className="text-lg mb-8 text-center">{t('championshipSelector.selectChampionship')}</h1>
 
       <div className="flex flex-col gap-4 w-full h-[560px] max-w-md px-6">
-        {selectedChampionships.map((championshipInternalName, index) => (
+        {selectedChampionships.map((championship, index) => (
           <button
             key={index}
             onClick={() =>
               engine.dispatch({
                 type: 'INIT_CHAMPIONSHIPS',
-                championshipInternalName,
+                championshipInternalName: championship.internalName,
               })
             }
             className="w-[342px] h-[80px] px-4 border-4 border-white text-lg uppercase transition hover:bg-white hover:text-[#3d7a33] disabled:opacity-50 disabled:cursor-not-allowed mx-auto"
           >
-            {championshipInternalName}
+            {championship.name}
           </button>
         ))}
       </div>
