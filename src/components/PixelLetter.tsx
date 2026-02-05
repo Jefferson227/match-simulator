7464220cb7e2fbd44d0234b2aa155dce8e1deb0c
Jefferson Matheus <jefferson227@gmail.com>
2026-02-04 23:11:46 -0500

    Create new component PixelLetter

---
+++ src/components/PixelLetter.tsx
@@ -0,0 +1,84 @@
+// Pixel art data for "WINNING PIXELS"
+const pixelData = {
+  W: [
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 1],
+  ],
+  I: [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  N: [
+    [1, 0, 0, 1],
+    [1, 1, 0, 1],
+    [1, 0, 1, 1],
+    [1, 0, 0, 1],
+    [1, 0, 0, 1],
+  ],
+  G: [
+    [1, 1, 1, 1],
+    [1, 0, 0, 0],
+    [1, 0, 1, 1],
+    [1, 0, 0, 1],
+    [1, 1, 1, 1],
+  ],
+  P: [
+    [1, 1, 1, 1],
+    [1, 0, 0, 1],
+    [1, 1, 1, 1],
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+  ],
+  X: [
+    [1, 0, 0, 1],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [1, 0, 0, 1],
+  ],
+  E: [
+    [1, 1, 1, 1],
+    [1, 0, 0, 0],
+    [1, 1, 1, 0],
+    [1, 0, 0, 0],
+    [1, 1, 1, 1],
+  ],
+  L: [
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+    [1, 1, 1, 1],
+  ],
+  S: [
+    [1, 1, 1, 1],
+    [1, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 1],
+    [1, 1, 1, 1],
+  ],
+};
+
+const PixelLetter: React.FC<{ letter: string }> = ({ letter }) => {
+  const pixels = pixelData[letter as keyof typeof pixelData];
+  if (!pixels) return null;
+
+  return (
+    <div className="grid gap-0.5 mx-1">
+      {pixels.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex gap-0.5">
+          {row.map((pixel, colIndex) => (
+            <div key={colIndex} className={`w-2 h-2 ${pixel ? 'bg-white' : 'bg-transparent'}`} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PixelLetter;
