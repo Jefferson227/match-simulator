2ddb6354c7604ded0c459ea282d0ddc979bc8057
Jefferson Matheus <jefferson227@gmail.com>
2026-02-04 23:12:14 -0500

    Use component PixelLetter

--- src/pages/InitialScreen/InitialScreen.tsx
+++ src/pages/InitialScreen/InitialScreen.tsx
@@ -3,6 +3,7 @@ import buildVersionData from '../../assets/build-version.json';
 import { useGameEngine } from '../../contexts/GameEngineContext';
 import { useGameState } from '../../services/useGameState';
 import MainLayout from '../../components/MainLayout/MainLayout';
+import PixelLetter from '../../components/PixelLetter';
 
 const InitialScreen: React.FC = () => {
   const [buildVersion, setBuildVersion] = useState('');
@@ -18,89 +19,6 @@ const InitialScreen: React.FC = () => {
       setBuildVersion('DEV');
     }
   }, []);
-
-  // Pixel art data for "WINNING PIXELS"
-  const pixelData = {
-    W: [
-      [1, 0, 0, 0, 1],
-      [1, 0, 0, 0, 1],
-      [1, 0, 1, 0, 1],
-      [1, 0, 1, 0, 1],
-      [1, 1, 0, 1, 1],
-    ],
-    I: [
-      [1, 1, 1],
-      [0, 1, 0],
-      [0, 1, 0],
-      [0, 1, 0],
-      [1, 1, 1],
-    ],
-    N: [
-      [1, 0, 0, 1],
-      [1, 1, 0, 1],
-      [1, 0, 1, 1],
-      [1, 0, 0, 1],
-      [1, 0, 0, 1],
-    ],
-    G: [
-      [1, 1, 1, 1],
-      [1, 0, 0, 0],
-      [1, 0, 1, 1],
-      [1, 0, 0, 1],
-      [1, 1, 1, 1],
-    ],
-    P: [
-      [1, 1, 1, 1],
-      [1, 0, 0, 1],
-      [1, 1, 1, 1],
-      [1, 0, 0, 0],
-      [1, 0, 0, 0],
-    ],
-    X: [
-      [1, 0, 0, 1],
-      [0, 1, 1, 0],
-      [0, 1, 1, 0],
-      [1, 0, 0, 1],
-    ],
-    E: [
-      [1, 1, 1, 1],
-      [1, 0, 0, 0],
-      [1, 1, 1, 0],
-      [1, 0, 0, 0],
-      [1, 1, 1, 1],
-    ],
-    L: [
-      [1, 0, 0, 0],
-      [1, 0, 0, 0],
-      [1, 0, 0, 0],
-      [1, 0, 0, 0],
-      [1, 1, 1, 1],
-    ],
-    S: [
-      [1, 1, 1, 1],
-      [1, 0, 0, 0],
-      [1, 1, 1, 1],
-      [0, 0, 0, 1],
-      [1, 1, 1, 1],
-    ],
-  };
-
-  const PixelLetter: React.FC<{ letter: string }> = ({ letter }) => {
-    const pixels = pixelData[letter as keyof typeof pixelData];
-    if (!pixels) return null;
-
-    return (
-      <div className="grid gap-0.5 mx-1">
-        {pixels.map((row, rowIndex) => (
-          <div key={rowIndex} className="flex gap-0.5">
-            {row.map((pixel, colIndex) => (
-              <div key={colIndex} className={`w-2 h-2 ${pixel ? 'bg-white' : 'bg-transparent'}`} />
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  };
 
   return (
     <MainLayout>
