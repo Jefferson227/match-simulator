2f7f898f5642be61a34101a9e2dbaf81c790ab7a
Jefferson Matheus <jefferson227@gmail.com>
2026-02-05 23:17:28 -0500

    Create TeamService file

---
+++ core/services/TeamService.ts
@@ -0,0 +1,21 @@
+import { Championship } from '../models/Championship';
+import { Team } from '../models/Team';
+import OperationResult from '../results/OperationResult';
+
+function getTeamsToSelect(championship: Championship): OperationResult<Team[]> {
+  try {
+    const result = new OperationResult(championship.startingTeams);
+    result.setSuccess();
+
+    return result;
+  } catch (error) {
+    const result = new OperationResult([] as Team[]);
+    const message = error instanceof Error ? error.message : String(error);
+    result.setError({ errorCode: 'exception', message });
+    return result;
+  }
+}
+
+export default {
+  getTeamsToSelect,
+};
