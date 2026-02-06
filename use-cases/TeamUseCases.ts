67526ce85bde8559d0a97b2353800db6e412e661
Jefferson Matheus <jefferson227@gmail.com>
2026-02-05 23:19:01 -0500

    Get teams from service

--- use-cases/TeamUseCases.ts
+++ use-cases/TeamUseCases.ts
@@ -1,11 +1,8 @@
-import ChampionshipContainer from '../core/models/ChampionshipContainer';
+import { Championship } from '../core/models/Championship';
 import { Team } from '../core/models/Team';
 import OperationResult from '../core/results/OperationResult';
+import TeamService from '../core/services/TeamService';
 
-export function getTeamsToSelect(
-  championshipContainer: ChampionshipContainer
-): OperationResult<Team[]> {
-  const result = new OperationResult([] as Team[]);
-  result.setError({ errorCode: 'implementation-missing', message: 'Implementation is missing.' });
-  return result;
+export function getTeamsToSelect(championship: Championship): OperationResult<Team[]> {
+  return TeamService.getTeamsToSelect(championship);
 }
