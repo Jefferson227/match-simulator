71d0f69561cd7c5244f0ac5be32a6eb3e684957a
Jefferson Matheus <jefferson227@gmail.com>
2026-02-15 02:18:03 -0500

    Remove unused reference

--- src/components/TeamMatchDetails/TeamMatchDetails.tsx
+++ src/components/TeamMatchDetails/TeamMatchDetails.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react';
+import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import utils from '../../utils/utils';
 import { Team } from '../../../core/models/Team';
