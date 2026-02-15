f9957bd006a11b80f5c78536e13bbe8aed8ba42a
Jefferson Matheus <jefferson227@gmail.com>
2026-02-15 02:17:14 -0500

    Add TODO

--- game-engine/game-engine.ts
+++ game-engine/game-engine.ts
@@ -147,6 +147,7 @@ export class GameEngine {
           },
         };
       case 'START_MATCHES':
+        // TODO: Implement logic to start the matches
         console.log('Starting matches!');
         return state;
       case 'SUBSTITUTE_PLAYER':
