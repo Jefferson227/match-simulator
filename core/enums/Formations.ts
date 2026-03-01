3ae5daa6c83977fed29c566e5baaeabf4800d411
Jefferson Matheus <jefferson227@gmail.com>
2026-03-01 18:34:01 -0500

    Add enum for formations

---
+++ core/enums/Formations.ts
@@ -0,0 +1,14 @@
+export const FORMATIONS = [
+  '5-3-2',
+  '3-5-2',
+  '4-4-2',
+  '4-3-3',
+  '4-2-4',
+  '5-4-1',
+  '3-4-3',
+  '3-3-4',
+] as const;
+
+type Formations = (typeof FORMATIONS)[number];
+
+export default Formations;
